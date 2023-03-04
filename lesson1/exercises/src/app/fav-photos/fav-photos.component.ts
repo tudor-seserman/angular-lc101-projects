f4b14849126e9755e18ec276629a07699a7f2946
Tudor <112909151+tudor-seserman@users.noreply.github.com>
2023-03-04 11:27:44 -0600

    29.7.2. Part 1: Modify the CSS

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Assignment Photos';
+  image1 = 'https://d1t35hkz8sx2bl.cloudfront.net/api/file/3dSlTYROTAKtqSjOLJni?cache=true';
+  image2 = 'https://pngimg.com/uploads/bagel/bagel_PNG6.png';
+  image3 = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Six_weeks_old_cat_(aka).jpg/320px-Six_weeks_old_cat_(aka).jpg';
 
   constructor() { }
 
