3d88bb4d77c92abcd7ef8c19166ebb75eeb33ebd
Tudor <112909151+tudor-seserman@users.noreply.github.com>
2023-03-05 15:08:08 -0600

    Final version

---
+++ lesson1/exercises/src/app/fav-links/fav-links.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fav-links',
+  templateUrl: './fav-links.component.html',
+  styleUrls: ['./fav-links.component.css']
+})
+export class FavLinksComponent implements OnInit {
+  favLinks =[
+    "https://www.launchcode.org/",
+    "https://www.hackerrank.com/"
+  ]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -10,7 +10,7 @@ export class FavPhotosComponent implements OnInit {
   image1 = 'https://d1t35hkz8sx2bl.cloudfront.net/api/file/3dSlTYROTAKtqSjOLJni?cache=true';
   image2 = 'https://pngimg.com/uploads/bagel/bagel_PNG6.png';
   image3 = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Six_weeks_old_cat_(aka).jpg/320px-Six_weeks_old_cat_(aka).jpg';
-
+  image4 = "https://upload.wikimedia.org/wikipedia/commons/2/2c/Rotating_earth_%28large%29.gif";
   constructor() { }
 
   ngOnInit() {
