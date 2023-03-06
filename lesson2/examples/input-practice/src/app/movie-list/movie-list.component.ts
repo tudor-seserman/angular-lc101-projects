6e3026a4c1a15f7b627cc45e3b0736733047f099
Tudor <112909151+tudor-seserman@users.noreply.github.com>
2023-03-06 13:29:54 -0600

    finished 2nd reading

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -12,4 +12,13 @@ export class MovieListComponent implements OnInit {
 
    ngOnInit() {
    }
+
+   addMovie (newTitle: string) {
+      if(!this.movies.includes(newTitle) && newTitle.length >0){
+         this.movies.push(newTitle);
+         return false
+      }else{
+         return true
+      }
+   }
 }
